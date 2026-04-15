760c44efb319fe1af4c72b228ea93cbaa0d99970
kuldeepraj <kr9667102@gmail.com>
2026-04-16 02:10:37 +0530

    Add Poppins font family to Tailwind CSS configuration

--- smart-agri/frontend/tailwind.config.js
+++ smart-agri/frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       colors: {
         agri: {
           dark:    '#1b4332',
